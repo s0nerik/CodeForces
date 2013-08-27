3e6f81bd7f7f64f5e54a55433b4f0b4380077048
s0nerik <sonerik@mail.ua>
2013-08-27 15:25:11 +0300

    339C. Ксюша и гири

--- Python/339C/solve.py
+++ Python/339C/solve.py
@@ -1,3 +1,25 @@
 import sys
-#f = sys.stdin
+sys.setrecursionlimit(100000)
+f = sys.stdin
 #f = open("input.txt", "r")
+w = f.readline().strip()
+m = int(f.readline())
+weight = []
+for i in range(len(w)):
+    if w[i] == "1":
+        weight.append(i+1)
+
+def solve(arr=[], last=-1, delta=0):
+    if len(arr) == m:
+        print("YES")
+        print(*arr)
+        exit(0)
+    else:
+        for i in weight:
+            if i != last:
+                if delta >= 0 and delta-i < 0:
+                    solve(arr + [i], i, delta-i)
+                elif delta < 0 and delta + i > 0:
+                    solve(arr + [i], i, delta+i)
+solve()
+print("NO")
